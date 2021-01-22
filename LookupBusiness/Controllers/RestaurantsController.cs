ebdc4e153a774678989ab0e89c444a971f3e5132
taylulz <taylulzcode@gmail.com>
2021-01-22 09:57:20 -0800

    add full CRUD routes. Working in Postman

--- LookupBusiness/Controllers/RestaurantsController.cs
+++ LookupBusiness/Controllers/RestaurantsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using LookupBusiness.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LookupBusiness.Controllers
 {
@@ -24,18 +25,36 @@ namespace LookupBusiness.Controllers
       return _db.Restaurants.ToList();
     }
 
-    // GET api/restaurants/3
+    // POST api/restaurants
+    [HttpPost]
+    public void Post([FromBody] Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+    }
+
+    // GET api/restaurants/1
     [HttpGet("{id}")]
     public ActionResult<Restaurant> Get(int id)
     {
       return _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
     }
 
-    // POST api/restaurants
-    [HttpPost]
-    public void Post([FromBody] Restaurant restaurant)
+    // PUT api/restaurants/1
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Restaurant restaurant)
     {
-      _db.Restaurants.Add(restaurant);
+      restaurant.RestaurantId = id;
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/restaurants/1
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var restaurantToDelete = _db.Restaurants.FirstOrDefault(entry => entry.RestaurantId == id);
+      _db.Restaurants.Remove(restaurantToDelete);
       _db.SaveChanges();
     }
   }
