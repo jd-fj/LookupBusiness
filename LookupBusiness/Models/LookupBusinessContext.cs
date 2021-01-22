e66d60629786dd9c1342aaa78fc1dcdfcc8fbdb5
taylulz <taylulzcode@gmail.com>
2021-01-22 08:39:36 -0800

    add commented out seed data section

--- LookupBusiness/Models/LookupBusinessContext.cs
+++ LookupBusiness/Models/LookupBusinessContext.cs
@@ -11,5 +11,21 @@ namespace LookupBusiness.Models
 
       public DbSet<Restaurant> Restaurants { get; set; }
       public DbSet<Shop> Shops { get; set; }
+
+      // protected override void OnMedelCreating(ModelBuilder builder)
+      // {
+      //   builder.Entity<Restaurant>()
+      //     .HasData(
+      //       //Restaurant Seed Data: Id, Name, Offerings, NearestIntersection, Pricing
+      //     );
+
+      //   builder.Entity<Shop>()
+      //     .HasData(
+      //       //Shop Seed Data: Id, Name, Offerings, NearestIntersection, Pricing
+      //     );
+      // }
+
   }
 }
+
+
