37486d657ee41843827f81a13933d82ed7e29fbb
taylulz <taylulzcode@gmail.com>
2021-01-22 08:28:49 -0800

    add restaurant and shop models

---
+++ LookupBusiness/Models/Shop.cs
@@ -0,0 +1,18 @@
+namespace LookupBusiness.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(200)]
+    public string Offerings { get; set; }
+    [Required]
+    public string NearestIntersection { get; set; }
+    [Required]
+    [Range(1, 5, ErrorMessage = "1 being affordable, 5 being super expensive")]
+    public int Pricing { get; set; }
+  }
+}
