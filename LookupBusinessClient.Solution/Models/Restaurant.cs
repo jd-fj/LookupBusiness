d3bb07fc3173aa4ba5c2bfc584195e3d7d47ab85
taylulz <taylulzcode@gmail.com>
2021-01-22 16:46:19 -0800

    append .Solution to client dir

---
+++ LookupBusinessClient.Solution/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Linq;
+// using System.Collections.Generic;
+
+// namespace MvcApiCall.Models
+// {
+//   public class Restaurant
+//   {
+//     public int RestaurantId { get; set; }
+//     public string Name { get; set; }
+//     public string Offerings { get; set; }
+//     public string NearestIntersection { get; set; }
+//     public int Pricing { get; set; }
+
+//     public static List<Restaurant> GetRestaurants()
+//     {
+      
+//     }
+//   }
+// }
